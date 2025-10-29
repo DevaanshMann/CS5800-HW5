470ec21a25bab365346f994427691de3080e2cf1
devaanshmann <devaanshmann5@gmail.com>
2025-10-29 13:22:07 -0700

    CombinedTest working fine

--- src/test/java/CombinedTest/CombinedTest.java
+++ src/test/java/CombinedTest/CombinedTest.java
@@ -1,9 +1,9 @@
-//package CombinedTest;
-//
-//import org.junit.platform.suite.api.SelectPackages;
-//import org.junit.platform.suite.api.Suite;
-//
-//@Suite
-//@SelectPackages({"Flyweight", "Proxy"})
-//public class CombinedTest {
-//}
+package CombinedTest;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"Flyweight", "Proxy"})
+public class CombinedTest {
+}
